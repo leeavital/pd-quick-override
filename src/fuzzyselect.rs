e39da50a2cd8b33d84c203ea995230228bc60878
Lee Avital <leeavital@gmail.com>
2023-02-10 13:05:20 -0500

    Refactor

--- src/fuzzyselect.rs
+++ src/fuzzyselect.rs
@@ -3,20 +3,20 @@ use std::{process::{Command, self, Stdio}, io::{Write, self, Read}};
 
 
 pub fn select(ss: Vec<String>) -> io::Result<String> {
-    let mut c = Command::new("fzf")
+    let mut subprocess = Command::new("fzf")
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .spawn()?;
 
-    
-        for s in ss {
-            c.stdin.as_mut().expect("could not get stdin").write(s.as_bytes())?;
-            c.stdin.as_mut().expect("could not get stdin").write("\n".as_bytes())?;
-        }
+    let stdin = subprocess.stdin.as_mut().unwrap();
+    for s in ss {
+        stdin.write(s.as_bytes())?;
+        stdin.write("\n".as_bytes())?;
+    }
 
-    c.wait()?;
+    subprocess.wait()?;
 
-    let mut stdout = c.stdout.unwrap();
+    let mut stdout = subprocess.stdout.unwrap();
     let mut output = String::new();
     stdout.read_to_string(&mut output)?;
 
