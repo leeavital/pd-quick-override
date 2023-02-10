d58cf7cf96558657e6c5630360c5bdce0143718f
Lee Avital <leeavital@gmail.com>
2023-02-10 14:26:32 -0500

    Simple client working (with dev key checked in)

---
+++ src/client.rs
@@ -0,0 +1,32 @@
+use serde::{Deserialize}; 
+
+#[derive(Deserialize, Debug)]
+pub struct UserResponse {
+    users: Vec<User>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct User {
+    id: String,
+    email: String,
+}
+
+
+pub async fn get_users() -> reqwest::Result<UserResponse> {
+
+    // let res = reqwest::get("https://httpbin.org/ip")
+    //     .await?;
+
+    let client = reqwest::Client::new();
+    let resp = client.get("https://api.pagerduty.com/users")
+        .header("Authorization", "Token token=y_NbAkKc66ryYTWUXYEu")
+        .header("Accept",  "application/vnd.pagerduty+json;version=2")
+        .header("Content-Type", "application/json")
+        .send()
+        .await?;
+
+
+    let users = resp.json::<UserResponse>().await?;
+
+    return Ok(users);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 
 mod fuzzyselect;
+mod client;
 
-fn main() {
-    println!("Hello, world!");
+
+#[tokio::main]
+async fn main() {
     let r = vec![String::from("hello"), String::from("goodbye")];
+
+    let u = client::get_users().await;
+
+    println!("{:?}", u.unwrap());
 
     let s = fuzzyselect::select(r).expect("could not read it");
     print!("{s}");
